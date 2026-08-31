2b7b917170751c8fa625ba04b08363314997031e
bassie1 <bassie423@gmail.com>
2026-08-31 13:34:54 -0400

    enforce business rules

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -38,6 +38,7 @@ namespace CPUFramework
                 try
                 {
                     SqlDataReader dr = cmd.ExecuteReader();
+                    CheckReturnValue(cmd);
                     if (loadtable == true)
                     {
                         dt.Load(dr);
@@ -55,6 +56,37 @@ namespace CPUFramework
             }
             SetAllColumnsAllowNull(dt);
             return dt;
+        }
+
+        private static void CheckReturnValue(SqlCommand cmd)
+        {
+            int returnvalue = 0;
+            string msg = "";
+            if (cmd.CommandType == CommandType.StoredProcedure)
+            {
+                foreach (SqlParameter p in cmd.Parameters)
+                {
+                    if (p.Direction == ParameterDirection.ReturnValue)
+                    {
+                        if (p.Value != null)
+                            returnvalue = (int)p.Value;
+                    }
+                    else if (p.ParameterName.ToLower() == "@message")
+                        if (p.Value != null)
+                        {
+                            msg = p.Value.ToString();
+                        }
+                }
+
+                if (returnvalue == 1)
+                {
+                    if (msg == "")
+                    {
+                        msg = $"{cmd.CommandText} did not do requested action.";
+                    }
+                    throw new Exception(msg);
+                }
+            }
         }
         public static DataTable GetDataTable(string sqlstatement)
         {
