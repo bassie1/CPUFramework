bd9e0062f07b3c2ac80014c46222f48c6fb547a8
bassie1 <bassie423@gmail.com>
2026-09-01 12:45:47 -0400

    update sproc

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System.ComponentModel.Design;
+using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Text;
@@ -26,6 +27,33 @@ namespace CPUFramework
         {
             return DoExecuteSql(cmd, true);
         }
+
+        public static void SaveDataRow(DataRow row, string sprocname)
+        {
+            SqlCommand cmd = GetSqlCommand(sprocname);
+            foreach (DataColumn col in row.Table.Columns)
+            {
+                string paramname = $"@{col.ColumnName}";
+                if (cmd.Parameters.Contains(paramname))
+                {
+                    cmd.Parameters[paramname].Value = row[col.ColumnName];
+                }
+            }
+            DoExecuteSql(cmd, false);
+
+            foreach (SqlParameter p in cmd.Parameters)
+            {
+                if (p.Direction == ParameterDirection.InputOutput)
+                {
+                    string colname = p.ParameterName.Substring(1);
+                    if (row.Table.Columns.Contains(colname))
+                    {
+                        row[colname] = p.Value;
+                    }
+                }
+            }
+        }
+
         private static DataTable DoExecuteSql(SqlCommand cmd, bool loadtable)
         {
 
